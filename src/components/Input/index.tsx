4d537896b262ebfed7d99885a007916c98a45bc1
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2021-09-29 19:20:51 -0300

    Arquivos alterados e typescript adicionado

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,22 @@
-import {
+import React, {
   useEffect,
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ElementType;
+}
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +54,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react';
+
+import { Header } from '../../components/Header';
+import { Food } from '../../components/Food';
+import { ModalAddFood } from '../../components/ModalAddFood';
+import { ModalEditFood } from '../../components/ModalEditFood';
+
+import { api } from '../../services/api';
+
+import { FoodInterface } from '../../types';
+
+import { FoodsContainer } from './styles';
+
+export function Dashboard() {
+  const [foods, setFoods] = useState<FoodInterface[]>([]);
+  const [editingFood, setEditingFood] = useState<FoodInterface>(
+    {} as FoodInterface
+  );
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    async function loadFoods() {
+      const response = await api.get<FoodInterface[]>('/foods');
+
+      setFoods(response.data);
+    }
+
+    loadFoods();
+  }, []);
+
+  async function handleAddFood(food: FoodInterface) {
+    try {
+      const response = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+      setFoods((oldState) => [...oldState, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleUpdateFood(food: FoodInterface) {
+    try {
+      const foodUpdated = await api.put(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+
+      const foodsUpdated = foods.map((f: FoodInterface) =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data
+      );
+
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleDeleteFood(id: number) {
+    await api.delete(`/foods/${id}`);
+
+    const foodsFiltered = foods.filter((food) => food.id !== id);
+
+    setFoods(foodsFiltered);
+  }
+
+  function toggleModal() {
+    setModalOpen(!modalOpen);
+  }
+
+  function toggleEditModal() {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  function handleEditFood(food: FoodInterface) {
+    setEditingFood(food);
+    setEditModalOpen(true);
+  }
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map((food) => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
